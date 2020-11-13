e0cce9e9939c9d30bc22e13eeac9c86238820567
Maciek <9256416+maciekwach@users.noreply.github.com>
2020-11-13 14:10:39 +0100

    Init commit

---
+++ coeat/src/main/java/com/gastro/sup/coeat/CoeatApplication.java
@@ -0,0 +1,13 @@
+package com.gastro.sup.coeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoeatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoeatApplication.class, args);
+    }
+
+}
---
+++ coeat/src/test/java/com/gastro/sup/coeat/CoeatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gastro.sup.coeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoeatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
